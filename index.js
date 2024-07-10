07fefd1740a62ccca74b1436f569f3136cedbbc5
Daniel Wagner <wagner.daniel.t@gmail.com>
2024-07-10 12:38:28 +0100

    add cors headers

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var MjpegProxy = require('mjpeg-proxy').MjpegProxy;
 var express = require('express');
+
 var app = express();
 var mjpegurl = process.env.MJPEG_URL;
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/index.jpg', new MjpegProxy(mjpegurl).proxyRequest);
 app.listen(process.env.MJPEG_PORT);
